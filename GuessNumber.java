2b7dfd8136ade47ae2e4cc130a162f773d16fde0
Tanvir Rahman <me@tanvirrahman.com>
2020-07-07 20:48:10 +0200

    Update GuessNumber.java

--- GuessNumber.java
+++ GuessNumber.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 public class GuessNumber {
-public static void main(String[] args) {
-// Generate a random number to be guessed
-int number = (int)(Math.random() * 101);
-Scanner input = new Scanner(System.in);
-System.out.println("Guess a number between 0 and 100");
-// Prompt the user to guess the number
-int guess = -1;
-while (guess != number){
-System.out.print("\nEnter your guess: ");
-guess = input.nextInt();
-if (guess == number)
-System.out.println("Yes, the number is " + number);
-else if (guess > number)
-System.out.println("Your guess is too high");
-else
-System.out.println("Your guess is too low");}
-}
+    public static void main(String[] args) {
+        // Generate a random number to be guessed
+        int number = (int)(Math.random() * 101);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a number between 0 and 100");
+        // Prompt the user to guess the number
+        int guess = -1;
+        while (guess != number) {
+            System.out.print("\nEnter your guess: ");
+            guess = input.nextInt();
+            if (guess == number)
+                System.out.println("Yes, the number is " + number);
+            else if (guess > number)
+                System.out.println("Your guess is too high");
+            else
+                System.out.println("Your guess is too low");
+        }
+    }
 }
